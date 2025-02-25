56fc540bc8bbbbee33d2270dba22872e6500ed17
202330093 <202330093@collegealma.ca>
2025-02-25 12:27:33 -0500

    labo sem 6 // q1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,5 @@ module org.calma.labo.labo_202330093 {
 
     opens org.calma.labo.labo_202330093 to javafx.fxml;
     exports org.calma.labo.labo_202330093;
+    exports org.calma.labo.labo_202330093.sem6;
 }
--- src/main/java/org/calma/labo/labo_202330093/sem6/q1.java
+++ src/main/java/org/calma/labo/labo_202330093/sem6/q1.java
@@ -1,6 +1,13 @@
 package org.calma.labo.labo_202330093.sem6;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class q1 extends Application {
 
@@ -10,6 +17,24 @@ public class q1 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        
+        Rectangle rec = new Rectangle(60,50,Color.GOLD);
+
+        HBox hbox = new HBox(rec);
+        hbox.setStyle("-fx-padding: 50 0 0 0;");
+        hbox.setSpacing(10);
+
+        Scene scene = new Scene(hbox,400,200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Q1");
+        primaryStage.show();
+
+        TranslateTransition anim = new TranslateTransition(Duration.seconds(1),rec);
+        anim.setFromX(0);
+        anim.setToX(scene.getWidth()-rec.getWidth());
+
+        anim.setCycleCount(ScaleTransition.INDEFINITE);
+        anim.setAutoReverse(true);
+        anim.play();
+
     }
 }
