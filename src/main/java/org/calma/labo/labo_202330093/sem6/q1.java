da2fe0f9fa6999bdebc9f1530a7e0d7ee1af87b8
202330093 <202330093@collegealma.ca>
2025-02-25 12:27:22 -0500

    labo sem 6 // q1

--- src/main/java/org/calma/labo/labo_202330093/sem6/q1.java
+++ src/main/java/org/calma/labo/labo_202330093/sem6/q1.java
@@ -1,4 +1,15 @@
 package org.calma.labo.labo_202330093.sem6;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class q1 {
+public class q1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
 }
